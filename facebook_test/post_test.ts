b7d84c2e8589cd0063d0fae3cda733dc8cc67c3e
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-06-26 10:52:54 +0700

    feat : add post validation and upload text file to post test

--- facebook_test/post_test.ts
+++ facebook_test/post_test.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import { makeFakeAvatar } from '../data-faker';
+import { makeFakeAvatar, makeFakeTextFile } from '../data-faker';
 
 dotenv.config({path:'./facebook_test/.env'});
 const email = process.env.FACEBOOK_EMAIL;
@@ -8,7 +8,7 @@ const baseURL = "https://id-id.facebook.com"
 
 Feature('post');
 
-const waitTime = 5;
+const waitTime = 10;
 
 Before(({ I }) => {
     // Login
@@ -25,6 +25,10 @@ Scenario('post text', async ({ I }) => { // Facebook doesn't handle spaces in em
     I.fillField(locate('div').withAttr({'role':'textbox'}),'test'); // Post textbox
     I.see('test');
     I.click('Kirim');
+    I.wait(waitTime);
+    I.click('Beranda');
+    I.wait(waitTime);
+    I.see('test');
 });
 
 Scenario('post text and photo', async ({ I }) => { // Facebook doesn't handle spaces in email
@@ -36,4 +40,23 @@ Scenario('post text and photo', async ({ I }) => { // Facebook doesn't handle sp
     I.click('Foto/video');
     I.attachFile('form input[type="file"]', 'testAvatar.jpg');
     I.click('Kirim');
+    I.wait(waitTime);
+    I.click('Beranda');
+    I.wait(waitTime);
+    I.see('test photo');
+});
+
+Scenario('upload non-image file on photo post', async ({ I }) => { // Facebook doesn't handle spaces in email
+    I.click('Buat');
+    I.click(locate('div').withAttr({'role':'button'}).withDescendant(locate('span').withText('Posting'))); // Posting button
+    I.fillField(locate('div').withAttr({'role':'textbox'}),'test photo'); // Post textbox
+    I.see('test photo');
+    await makeFakeTextFile('fakeText.txt');
+    I.click('Foto/video');
+    I.attachFile('form input[type="file"]', 'fakeText.txt'); // File is uploaded but no picture is shown
+    I.click('Kirim');
+    I.wait(waitTime);
+    I.click('Beranda');
+    I.wait(waitTime);
+    I.see('test photo');
 });
