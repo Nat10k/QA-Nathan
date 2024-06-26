078046f623c23c6720368a38387f10da83ac4a9d
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-06-26 14:17:06 +0700

    feat : open game scenario

---
+++ facebook_test/game_test.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({path:'./facebook_test/.env'});
+const email = process.env.FACEBOOK_EMAIL;
+const password = process.env.FACEBOOK_PASSWORD;
+const baseURL = "https://id-id.facebook.com"
+
+Feature('game');
+
+const waitTime = 10;
+
+Before(({ I }) => {
+    // Login
+    I.amOnPage(baseURL);
+    I.fillField('email', email);
+    I.fillField('pass', password);
+    I.click('Masuk');
+    I.wait(waitTime);
+});
+
+Scenario('open a game (cooking center)', async ({ I }) => {
+    I.amOnPage('https://www.facebook.com/gaming/play/?store_visit_source=games_bookmark');
+    I.click('Mainkan Game');
+    I.click('Cooking Center');
+    I.wait(10);
+    I.see('SEDANG BERMAIN');
+});
