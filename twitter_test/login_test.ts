97bfd34cf7c0421d60210358ec3ff108f83c371f
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-06-28 17:19:46 +0700

    feat : twitter login test

---
+++ twitter_test/login_test.ts
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({path:'./twitter_test/.env'});
+const email = process.env.TWITTER_EMAIL;
+const password = process.env.TWITTER_PASSWORD;
+const baseURL = "https://x.com/?lang=en"
+
+Feature('login');
+
+const waitTime = 10;
+
+Before(({ I }) => {
+    I.amOnPage(baseURL);
+})
+
+Scenario('wrong credentials', async ({ I }) => {
+    // Wrong email
+    I.click('Sign in');
+    I.click('Next');
+    I.see('Sorry, we could not find your account.');
+    I.fillField('Phone, email, or username', 'lolololololol@gmail.com');
+    I.click('Next');
+    I.see('Sorry, we could not find your account.');
+
+    // Wrong password
+    I.fillField('Phone, email, or username', email);
+    I.fillField('password', 'blidaiubwehbsa');
+    I.click('Log in');
+    I.waitForText('Wrong password', waitTime);
+});
+
+Scenario('complete login',  async ({ I }) => {
+    I.fillField('Phone, email, or username', email);
+    I.fillField('password', password);
+    I.click('Log in');
+    I.waitForText('What is happening?!',waitTime);
+});
