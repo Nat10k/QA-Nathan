1aca8baf6624ec093afe9841e26cfe55e9ed4c5a
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-07-02 10:00:31 +0700

    refactor : step definition comments

--- step_definitions/steps.ts
+++ step_definitions/steps.ts
@@ -8,23 +8,23 @@ const { I } = inject();
 // Add in your custom step files
 
 Given('I am on the login page', () => {
-  // From "features\basic.feature" {"line":7,"column":5}
+  // From "features\facebook_login.feature" {"line":7,"column":5}
   I.amOnPage('/');
 });
 
 When('I enter my username and password', () => {
-  // From "features\basic.feature" {"line":8,"column":5}
+  // From "features\facebook_login.feature" {"line":8,"column":5}
   I.fillField('email', email);
   I.fillField('pass', password);
 });
 
 When('I click Log In', () => {
-  // From "features\basic.feature" {"line":9,"column":5}
+  // From "features\facebook_login.feature" {"line":9,"column":5}
   I.click('Log in');
   I.wait(5);
 });
 
 Then('I should see my Facebook dashboard', () => {
-  // From "features\basic.feature" {"line":9,"column":5}
+  // From "features\facebook_login.feature" {"line":9,"column":5}
   I.see('Selamat datang di Facebook');
 });
