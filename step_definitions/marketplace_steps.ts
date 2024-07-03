21457e0442764da63b11e17f40429169fd48f04a
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-07-03 11:50:50 +0700

    feat : see seller detail

--- step_definitions/marketplace_steps.ts
+++ step_definitions/marketplace_steps.ts
@@ -29,7 +29,7 @@ Given('I am on the marketplace page', () => {
 
 When('I click one of the items', () => {
   // From "features\facebook_marketplace.feature" {"line":13,"column":5}
-  I.click('a.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1sur9pj.xkrqix3.x1lku1pv:nth-child(1)');
+  I.click(locate('a').inside(locate('div').withClassAttr('x8gbvx8 x78zum5 x1q0g3np x1a02dak x1nhvcw1 x1rdy4ex xcud41i x4vbgl9 x139jcc6')));
 });
 
 Then('I should see details of the item', () => {
@@ -40,7 +40,7 @@ Then('I should see details of the item', () => {
 Given('I am on an item detail page', () => {
   // From "features\facebook_marketplace.feature" {"line":19,"column":5}
   I.amOnPage('/marketplace/?ref=bookmark');
-  I.click('a.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1sur9pj.xkrqix3.x1lku1pv:nth-child(1)');
+  I.click(locate('a').inside(locate('div').withClassAttr('x8gbvx8 x78zum5 x1q0g3np x1a02dak x1nhvcw1 x1rdy4ex xcud41i x4vbgl9 x139jcc6')));
   I.waitForText('Detail', 10);
 });
 
