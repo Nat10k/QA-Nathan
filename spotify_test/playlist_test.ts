7d185f832541d0ae5c200c44ff8bbb18248b3999
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-07-08 11:33:40 +0700

    feat : remove songs from playlist scenario

--- spotify_test/playlist_test.ts
+++ spotify_test/playlist_test.ts
@@ -41,24 +41,35 @@ Scenario('edit the playlist details', async ({ I }) => {
     I.attachFile(locate('input').withAttr({type:'file','data-testid':'image-file-picker'}).inside(editModal), 'fakeAvatar.jpg');
     I.wait(5);
     // Change playlist name
-    I.fillField(locate('input').withAttr({type:'text', 'data-testid':'playlist-edit-details-name-input'}).inside(editModal), 'New Playlist');
-    I.fillField(locate('textarea').withAttr({'data-testid':'playlist-edit-details-description-input'}).inside(editModal), 'New Playlist Description');
+    I.fillField(locate('input').withAttr({type:'text', 'data-testid':'playlist-edit-details-name-input'}).inside(editModal), 'Test Playlist');
+    I.fillField(locate('textarea').withAttr({'data-testid':'playlist-edit-details-description-input'}).inside(editModal), 'Test Playlist Description');
     I.click(locate('button').withAttr({'data-testid':'playlist-edit-details-save-button'}).inside(editModal));
-    I.waitForText('New Playlist', 5);
+    I.waitForText('Test Playlist', 5);
 });
 
 Scenario('play the playlist', async ({ I }) => {
     I.click(locate('div').withAttr({role:'button'}).inside(locate('ul').withAttr({role:'list', 'aria-label':'Your Library'})));
-    I.waitForText('New Playlist', 10);
+    I.waitForText('Test Playlist', 10);
     I.click(locate('button').withAttr({'data-testid':'play-button'}));
     I.waitForElement(locate('aside').withAttr({'aria-label':'Now playing view'}), 30);
+});
+
+Scenario('remove songs from the playlist', async ({ I }) => {
+    I.click(locate('div').withAttr({role:'button'}).inside(locate('ul').withAttr({role:'list', 'aria-label':'Your Library'})));
+    I.waitForText('Test Playlist', 10);
+    for (let i=0; i<3; i++) {
+        I.rightClick(locate('div').withAttr({'data-testid':'tracklist-row'}).
+                                 inside(locate('div').withAttr({'data-testid':'playlist-tracklist'})).at(1));
+        I.click(locate('button').withAttr({role:'menuitem'}).inside(locate('div').withAttr({'data-testid':'context-menu'})).
+                                 withChild(locate('span').withText('Remove from this playlist')));
+    }
 });
 
 Scenario('delete playlist', async ({ I }) => {
     I.rightClick(locate('div').withAttr({role:'button'}).inside(locate('ul').withAttr({role:'list', 'aria-label':'Your Library'})));
     I.click(locate('button').withAttr({role:'menuitem'}).inside(locate('div').withAttr({'data-testid':'context-menu'})).
             withChild(locate('span').withText('Delete')));
-    I.click(locate('button').withChild(locate('span').withText('Delete')).inside(locate('div').withAttr({role:'dialog', 'aria-label':'Delete New Playlist?'})));
+    I.click(locate('button').withChild(locate('span').withText('Delete')).inside(locate('div').withAttr({role:'dialog', 'aria-label':'Delete Test Playlist?'})));
 });
 
 export {}
