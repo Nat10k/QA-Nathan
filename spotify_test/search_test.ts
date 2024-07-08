19c729991b1126096c507d0f9d5c36d28c2bcf8b
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-07-08 16:12:09 +0700

    feat : really long text search spotify

--- spotify_test/search_test.ts
+++ spotify_test/search_test.ts
@@ -1,3 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config({path:'./spotify_test/.env'});
+
+const longText = process.env.LONG_TEXT;
+
 Feature('spotify search').tag('@spotify');
 
 Before(({ login }) => {
@@ -22,5 +27,11 @@ Scenario('choose a category filter', async ({ I }) => {
     I.click(locate('a').withDescendant(locate('span').withText('Albums')));
     I.waitForText('Ado "Ready For My Show Playlist"', 20, '#searchPage');
 });
+
+Scenario('really long search', async ({ I }) => { // Terdapat batas panjang karakter di searchbox
+    I.click(locate('a').withAttr({'aria-label':'Search'}));
+    I.fillField(locate('input').inside(locate('form').withAttr({role:'search'})), longText);
+    I.waitForText('No results found', 5);
+})
 
 export {}
